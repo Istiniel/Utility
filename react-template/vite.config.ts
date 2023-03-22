4d46f6f79973a04872800c945eda2e8a4686144f
Istiniel <eclipsio@mail.ru>
2023-03-22 15:58:02 +0300

    feat: add react template

---
+++ react-template/vite.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+import react from '@vitejs/plugin-react';
+import { configDefaults, defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.ts'],
+    coverage: {
+      exclude: [...configDefaults.coverage.exclude, 'src/main.tsx'],
+      all: true,
+      src: ['src'],
+      provider: 'c8',
+      reporter: ['text'],
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/abstracts/_index.scss";`,
+      },
+    },
+  },
+});
